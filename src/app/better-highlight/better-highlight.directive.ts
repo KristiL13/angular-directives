1692e618f47fd0cb6bd2b21732f3bcc6466bbe6d
Kristi Laur <kristi@heisi.eu>
2022-05-03 08:29:48 +0300

    Use HostListener to liste to host events

--- src/app/better-highlight/better-highlight.directive.ts
+++ src/app/better-highlight/better-highlight.directive.ts
@@ -1,14 +1,22 @@
-import { Directive, ElementRef, OnInit, Renderer2 } from '@angular/core';
+import { Directive, ElementRef, HostListener, OnInit, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[appBetterHighlight]'
 })
 export class BetterHighlightDirective implements OnInit {
-
   constructor(private elRef: ElementRef, private renderer: Renderer2) { }
 
   // renderer is better way to access elements, because Angular can also run services and not in the browser.
   ngOnInit(): void {
-      this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'lightblue' /*, '!important' */);
+    // this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'lightblue' /*, '!important' */);
+  }
+  @HostListener('mouseenter') mouseover(eventData: Event) {
+    // sulgudes decoratori järel saab kasuta kõiki saadaval olevaid evente
+    // sulgudes mu eventlisteneri järel saab kasutada ka custom event'e nagu mujal
+    this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'lightblue');
+  }
+
+  @HostListener('mouseleave') mouseleave() {
+    this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'transparent');
   }
 }

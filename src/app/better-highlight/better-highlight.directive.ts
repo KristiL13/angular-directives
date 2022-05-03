de6e6e9e3332154801948eb7a7f3e64565fe12b8
Kristi Laur <kristi@heisi.eu>
2022-05-03 08:43:30 +0300

    Directives: Use HostBinding to bind to host properties

--- src/app/better-highlight/better-highlight.directive.ts
+++ src/app/better-highlight/better-highlight.directive.ts
@@ -1,9 +1,16 @@
-import { Directive, ElementRef, HostListener, OnInit, Renderer2 } from '@angular/core';
+import { Directive, ElementRef, HostBinding, HostListener, OnInit, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[appBetterHighlight]'
 })
 export class BetterHighlightDirective implements OnInit {
+  @HostBinding('style.backgroundColor') backgroundColor: string = 'transparent'; 
+  // Siin sulgudes märgin, millist property-t host elemendist ma siduda tahan.
+  // Sisuliselt on see sama, mille poole pöördun basic-highlight.directive.ts failis.
+  // camelCase on siin oluline backgroundColor kirjutamisel, kuna me pääseme ligi DOM property-le, mis ei tea -.
+  // Kui siin kusagil allpool nüüd määran oma kohalikule backgroundColor muutujale väärtuse, siis vastavalt mu
+  // tingimustele muutub hostis style.backgroundColor väärtus
+
   constructor(private elRef: ElementRef, private renderer: Renderer2) { }
 
   // renderer is better way to access elements, because Angular can also run services and not in the browser.
@@ -13,10 +20,12 @@ export class BetterHighlightDirective implements OnInit {
   @HostListener('mouseenter') mouseover(eventData: Event) {
     // sulgudes decoratori järel saab kasuta kõiki saadaval olevaid evente
     // sulgudes mu eventlisteneri järel saab kasutada ka custom event'e nagu mujal
-    this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'lightblue');
+    // this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'lightblue');
+    this.backgroundColor = 'lightgreen';
   }
 
   @HostListener('mouseleave') mouseleave() {
-    this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'transparent');
+    // this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'transparent');
+    this.backgroundColor = 'transparent';
   }
 }
